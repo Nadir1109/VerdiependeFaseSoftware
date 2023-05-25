8104a5f90dfd50bd5bfe9efb708a8e927ff132a9
Nadir Regragui <nadir.regragui@edu-kw1c.nl>
2023-05-26 01:55:00 +0200

    listview van Players is gevuld

--- VerdiependeFaseSoftware/UI/PlayerOverview.cs
+++ VerdiependeFaseSoftware/UI/PlayerOverview.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VerdiependeFaseSoftware.Classes;
 
 namespace VerdiependeFaseSoftware.UI
 {
     public partial class PlayerOverview : Form
     {
+        private DatabaseConnection _conn;
         public PlayerOverview()
         {
             InitializeComponent();
@@ -30,7 +33,37 @@ namespace VerdiependeFaseSoftware.UI
             lvwPlayerOverview.Columns.Add("Team", 100);
             lvwPlayerOverview.View = View.Details;
 
+            _conn = new DatabaseConnection();
 
+            if (_conn.Connect())
+            {
+                List<Player> players = _conn.GetPlayers();
+
+                foreach (Player player in players)
+                {
+                    ListViewItem item = new ListViewItem(player.playerID.ToString());
+                    item.SubItems.Add(player.playerName);
+                    item.SubItems.Add(player.attack.ToString());
+                    item.SubItems.Add(player.control.ToString());
+                    item.SubItems.Add(player.defend.ToString());
+                    item.SubItems.Add(player.position);
+                    item.SubItems.Add(player.nationality);
+                    item.SubItems.Add(player.rating.ToString());
+                    item.SubItems.Add(player.teamID.ToString());
+
+                    lvwPlayerOverview.Items.Add(item);
+                }
+
+                _conn.Disconnect();
+            }
+            else
+            {
+                MessageBox.Show("Fout bij het verbinden met de database. Controleer uw databaseverbinding en probeer het opnieuw.");
+            }
+        }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _conn?.Disconnect();
         }
     }
 }
